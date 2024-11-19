337c0708d31c00c4ea589284713b70a339357ce3
winatabayu <winatabayu01@gmail.com>
2024-11-19 22:11:05 +0700

    feat: add new select option -- account list by user login -- category list by user login

--- app/Http/Controllers/Api/SelectOption/GlobalSelectOptionController.php
+++ app/Http/Controllers/Api/SelectOption/GlobalSelectOptionController.php
@@ -6,13 +6,22 @@ use App\Enums\Account\AccountType;
 use App\Enums\Category\CategoryType;
 use App\Enums\Transaction\TransactionType;
 use App\Http\Controllers\Api\Controller;
+use App\Models\Account\Account;
+use App\Models\Category\Category;
+use App\Queries\Account\AccountQuery;
+use App\Queries\Category\CategoryQuery;
 use Dentro\Yalr\Attributes;
 use Illuminate\Http\Request;
+use Winata\Core\Response\Http\Response;
 
 #[Attributes\Prefix('')]
 #[Attributes\Name('', true, false)]
 class GlobalSelectOptionController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return Response
+     */
     #[Attributes\Get(uri: 'account-types')]
     public function accountTypes(Request $request): \Winata\Core\Response\Http\Response
     {
@@ -28,6 +37,10 @@ class GlobalSelectOptionController extends Controller
         return $this->response($data);
     }
 
+    /**
+     * @param Request $request
+     * @return Response
+     */
     #[Attributes\Get(uri: 'category-types')]
     public function categoryTypes(Request $request): \Winata\Core\Response\Http\Response
     {
@@ -43,6 +56,10 @@ class GlobalSelectOptionController extends Controller
         return $this->response($data);
     }
 
+    /**
+     * @param Request $request
+     * @return Response
+     */
     #[Attributes\Get(uri: 'transaction-types')]
     public function transactionTypes(Request $request): \Winata\Core\Response\Http\Response
     {
@@ -53,6 +70,52 @@ class GlobalSelectOptionController extends Controller
                 return [
                     'id' => $key,
                     'name' => $value,
+                ];
+            });
+        return $this->response($data);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    #[Attributes\Get(uri: 'categories')]
+    public function categories(Request $request): \Winata\Core\Response\Http\Response
+    {
+        $user = auth()->user();
+        $accounts = CategoryQuery::byUser($user->id)
+            ->filterColumn()
+            ->orderColumn()
+            ->getAllData();
+
+        $data = collect($accounts)
+            ->map(function (Category $data) {
+                return [
+                    'id' => $data->id,
+                    'name' => $data->name,
+                ];
+            });
+        return $this->response($data);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    #[Attributes\Get(uri: 'accounts')]
+    public function accounts(Request $request): \Winata\Core\Response\Http\Response
+    {
+        $user = auth()->user();
+        $accounts = AccountQuery::byUser($user->id)
+            ->filterColumn()
+            ->orderColumn()
+            ->getAllData();
+
+        $data = collect($accounts)
+            ->map(function (Account $data) {
+                return [
+                    'id' => $data->id,
+                    'name' => $data->name,
                 ];
             });
         return $this->response($data);
