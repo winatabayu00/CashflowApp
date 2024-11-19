bb6dc9cb966cd2687ee077f6a0935be3a4a6502c
winatabayu <winatabayu01@gmail.com>
2024-11-19 21:26:03 +0700

    feat: category -- create migration -- create model -- create action -- create controller -- create select option for category type

---
+++ database/migrations/2024_11_19_135634_create_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignIdFor(\App\Models\User::class, 'user_id')
+                ->constrained()
+                ->onDelete('restrict');
+            $table->string('name');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
